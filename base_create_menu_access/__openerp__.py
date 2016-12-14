10c9e70729a41b8fb78312ed5ddf16e758e54888
mike <viriyananda.michael@gmail.com>
2016-12-14 10:09:52 +0700

    Revision

--- base_create_menu_access/__openerp__.py
+++ base_create_menu_access/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Base Create Menu Access",
-    "version": "8.0.1.1.1",
+    "version": "8.0.2.1.2",
     "summary": "Adds wizard to create menu access",
     "category": "Base",
     "website": "https://opensynergy-indonesia.com",
@@ -12,6 +12,7 @@
     "installable": True,
     "depends": ["base"],
     "data": [
+        "views/ir_ui_menu.xml",
         "wizards/base_create_menu_access_view.xml"
     ],
 }
