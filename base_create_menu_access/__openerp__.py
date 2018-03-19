9b2e9dc6952b7b3e75646651dd806b29130b090f
mike <viriyananda.michael@gmail.com>
2018-03-19 15:28:44 +0700

    [UPD] 8.0.3.0.0

--- base_create_menu_access/__openerp__.py
+++ base_create_menu_access/__openerp__.py
@@ -3,15 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Base Create Menu Access",
-    "version": "8.0.2.1.4",
+    "version": "8.0.3.0.0",
     "summary": "Adds wizard to create menu access",
     "category": "Base",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["base"],
+    "depends": [
+        "base"
+    ],
     "data": [
+        "data/ir_module_category.xml",
+        "data/ir_cron.xml",
         "views/ir_ui_menu.xml",
         "wizards/base_create_menu_access_view.xml"
     ],
--- base_create_menu_access/models/ir_ui_menu.py
+++ base_create_menu_access/models/ir_ui_menu.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
 
-from openerp import models, fields
+from openerp import models, fields, api
+from openerp.tools import SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
 
 
 class IrUiMenu(models.Model):
@@ -12,3 +16,64 @@ class IrUiMenu(models.Model):
         string="Custom Group",
         comodel_name="res.groups"
     )
+
+    @api.multi
+    def _check_custom_group(self):
+        self.ensure_one()
+        result = False
+        if self.custom_group_id:
+            result = True
+        return result
+
+    @api.multi
+    def _prepare_data_group(self):
+        self.ensure_one()
+        category_id = self.env.ref(
+            "base_create_menu_access.custom_menu_access_module_category_data"
+        )
+        value = {
+            "name": self.display_name,
+            "category_id": category_id.id,
+            "users": [(6, 0, [SUPERUSER_ID])]
+        }
+        return value
+
+    @api.multi
+    def set_menu_access(self):
+        self.ensure_one()
+        obj_res_groups = self.env["res.groups"]
+        group = self._check_custom_group()
+
+        if not group and self.action:
+            self.groups_id = [(6, 0, [])]
+            data_group = self._prepare_data_group()
+            group = obj_res_groups.create(data_group)
+            self.write({
+                "custom_group_id": group.id,
+                "groups_id": [(6, 0, [group.id])],
+            })
+
+    @api.model
+    def cron_update_menu_access(self):
+        logging.info(u"Update menu access")
+
+        menu_setting =\
+            self.env.ref("base.menu_administration")
+
+        criteria_root = [
+            ("parent_id", "=", False),
+            ("id", "<>", menu_setting.id)
+        ]
+        root_menu_ids = self.search(criteria_root)
+
+        for root_menu in root_menu_ids:
+            context = {"ir.ui.menu.full_list": True}
+            criteria_child = [
+                ("id", "child_of", root_menu.id),
+                ("id", "<>", root_menu.id)
+            ]
+            menu_ids = self.with_context(
+                context
+            ).search(criteria_child)
+            for menu in menu_ids:
+                menu.set_menu_access()
