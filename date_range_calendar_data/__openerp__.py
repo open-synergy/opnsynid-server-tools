44b1e5a4c2665e7b3554636a67d728cb1ec877ca
Andhitia Rama <andhitia.r@gmail.com>
2018-09-16 16:42:31 +0700

    [8.0.1.0.0] date_range_calendar_data

---
+++ date_range_calendar_data/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Date Range Data - Calendar",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "category": "Generic Modules/Base",
+    "website": "https://opensynergy-indonesia.com",
+    "depends": [
+        "date_range"
+    ],
+    "data": [
+        "data/date_range_type_data.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
