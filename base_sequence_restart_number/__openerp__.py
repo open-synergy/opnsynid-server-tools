263bd83839e9cfdd857fe6f87c0cae258182b50f
Andhitia Rama <andhitia.r@gmail.com>
2018-09-16 18:50:25 +0700

    [LINT] base_sequence_restart_number

--- base_sequence_restart_number/__openerp__.py
+++ base_sequence_restart_number/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Restart Sequence",
     "version": "8.0.1.0.0",
