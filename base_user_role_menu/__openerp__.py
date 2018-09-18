72389c0f4bdf7a14a3fc97f974f6033947396210
Andhitia Rama <andhitia.r@gmail.com>
2018-09-18 11:19:21 +0700

    [8.0.1.0.0] base_user_role_menu

---
+++ base_user_role_menu/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Manage Menu Access Based on Roles",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "category": "Generic Modules/Base",
+    "website": "https://opensynergy-indonesia.com",
+    "depends": [
+        "base_user_role"
+    ],
+    "data": [
+        "data/ir_actions_server_data.xml",
+        "views/ir_ui_menu_views.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
