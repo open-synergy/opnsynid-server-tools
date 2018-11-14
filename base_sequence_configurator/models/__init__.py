ba4e535b39605252a611d5ad3bc43e6b18c7050d
Andhitia Rama <andhitia.r@gmail.com>
2018-11-14 14:06:10 +0700

    [12.0.0.0] base_sequence_configurator

---
+++ base_sequence_configurator/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    base_sequence_configurator,
+    base_sequence_object,
+    )
