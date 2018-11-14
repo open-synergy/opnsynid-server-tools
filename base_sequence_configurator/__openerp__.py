6afdc135c8552a0cb23373dd6cccceac44027003
Andhitia Rama <andhitia.r@gmail.com>
2018-11-14 20:53:59 +0700

    [8.0.1.0.1] base_sequence_configurator

--- base_sequence_configurator/__openerp__.py
+++ base_sequence_configurator/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Configurable Sequence Generator",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Administration",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
