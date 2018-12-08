52dee03ab7030dee59a17b78b55fb442d84a005b
Andhitia Rama <andhitia.r@gmail.com>
2018-12-08 22:22:12 +0700

    [12.0.1.0.0] base_workflow_policy
    Migrate log:
    * [8.0.1.0.0] base_workflow_policy
    * [8.0.1.0.1] base_workflow_policy

---
+++ base_workflow_policy/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    base_workflow_policy,
+    base_workflow_policy_object,
+)
---
+++ base_workflow_policy/models/base_workflow_policy_object.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, SUPERUSER_ID
+
+
+class BaseWorkflowPolicyObject(models.AbstractModel):
+    _name = "base.workflow_policy_object"
+    _description = "Workflow Policy Object"
+
+    @api.multi
+    def _compute_policy(self):
+        obj_line = self.env["base.workflow_policy_line"]
+        criteria = [
+            ("workflow_id.model_id.model", "=", str(self._model)),
+        ]
+        policy_lines = obj_line.search(criteria)
+        for document in self:
+            for policy in policy_lines:
+                if self.env.user.id == SUPERUSER_ID:
+                    result = True
+                else:
+                    result = policy.\
+                        get_policy(
+                            document)
+                setattr(
+                    document,
+                    policy.field_id.name,
+                    result,
+                )
