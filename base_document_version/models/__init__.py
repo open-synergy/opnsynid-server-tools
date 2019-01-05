00fa9e5a9e2ab806e55eca751f30a982b9cac225
Andhitia Rama <andhitia.r@gmail.com>
2019-01-05 17:47:24 +0700

    [8.0.1.0.0] base_document_version

---
+++ base_document_version/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import base_document_version
+# from . import base_workflow_policy_object
