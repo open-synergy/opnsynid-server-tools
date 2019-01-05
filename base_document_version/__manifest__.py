79383e72c541dd0f40c149fbb2ec979dbd5bf80d
Andhitia Rama <andhitia.r@gmail.com>
2019-01-05 17:47:24 +0700

    [12.0.1.0.0] base_document_version
    Migration history:
    * [8.0.1.0.0] base_document_version

---
+++ base_document_version/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018-2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Generic Mechanism for Document Version",
+    "version": "12.0.1.0.0",
+    "category": "Administration",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+}
