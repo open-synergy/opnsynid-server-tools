74da6a7dc002843300cc0d308da7edd720fdd969
Andhitia Rama <andhitia.r@gmail.com>
2019-01-29 22:25:57 +0700

    [8.0.1.0.0] base_qr_code

---
+++ base_qr_code/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Abstract Class for QR Code",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "external_dependencies": {
+        "python": [
+            "qrcode",
+        ],
+    },
+    "data": [
+    ],
+}
