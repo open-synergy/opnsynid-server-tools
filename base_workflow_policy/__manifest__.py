a3f3744fbf13ae115642e7e5d2aa740993bc7663
Andhitia Rama <andhitia.r@gmail.com>
2019-02-03 08:34:20 +0700

    [12.0.1.0.0] base_workflow_policy
    * Use _name instead of _model
    * Fix lint

--- base_workflow_policy/__manifest__.py
+++ base_workflow_policy/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Abstract Model for Workflow Policy",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "category": "Administration",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
