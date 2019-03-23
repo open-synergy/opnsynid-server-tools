186f4bac93c1e29f2d6d8d4589fe04fcc0ded3be
Andhitia Rama <andhitia.r@gmail.com>
2019-03-23 19:06:27 +0700

    [8.0.1.0.2] base_qr_code
    * Add import error

--- base_qr_code/__openerp__.py
+++ base_qr_code/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Abstract Class for QR Code",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.0.2",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
