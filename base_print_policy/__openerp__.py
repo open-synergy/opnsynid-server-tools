27abeabea4757f03f375190e9031047e8b7dda22
mikevhe18 <viriyananda.michael@gmail.com>
2019-05-31 18:00:50 +0700

    [8.0.1.0.0][base_print_policy]

---
+++ base_print_policy/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Base Print Policy",
+    "version": "8.0.1.0.0",
+    "category": "Base",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/base_print_policy_view.xml",
+        "wizards/base_print_document.xml"
+    ],
+}
---
+++ base_print_policy/wizards/base_print_document.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api, fields
+from openerp.tools.safe_eval import safe_eval as eval
+
+
+class BasePrintDocument(models.TransientModel):
+    _name = "base.print.document"
+    _description = "Base Print Document"
+
+    @api.model
+    def _compute_allowed_print_action_ids(self):
+        result = []
+        obj_print_policy =\
+            self.env["base.print.policy"]
+        active_model = self.env.context.get("active_model", "")
+        criteria = [
+            ("report_action_id.model", "=", active_model)
+        ]
+        print_policy_ids = obj_print_policy.search(criteria)
+        if print_policy_ids:
+            for print_policy in print_policy_ids:
+                policy =\
+                    self.get_print_policy(
+                        print_policy.python_condition)
+                if policy:
+                    result.append(print_policy.report_action_id.id)
+        return result
+
+    allowed_print_action_ids = fields.Many2many(
+        string="Allowed Print Action",
+        comodel_name="ir.actions.report.xml",
+        default=lambda self: self._compute_allowed_print_action_ids(),
+        relation="rel_print_document_2_action_report",
+        column1="wizard_id",
+        column2="report_action_id",
+    )
+
+    report_action_id = fields.Many2one(
+        string="Report Template",
+        comodel_name="ir.actions.report.xml",
+        required=True,
+    )
+
+    @api.multi
+    def _get_object_id(self):
+        active_ids = self.env.context.get("active_ids", [])
+        active_model = self.env.context.get("active_model", "")
+        object_id = self.env[active_model].browse(
+            active_ids
+        )
+        return object_id
+
+    @api.multi
+    def _get_localdict(self):
+        return {
+            "record": self._get_object_id()
+        }
+
+    @api.multi
+    def get_print_policy(self, python_condition):
+        localdict = self._get_localdict()
+
+        try:
+            eval(python_condition,
+                 localdict, mode="exec", nocopy=True)
+            result = localdict["result"]
+        except:
+            result = True
+
+        return result
+
+    @api.multi
+    def action_print(self):
+        object_id = self._get_object_id()
+        return self.env['report'].get_action(
+            object_id, self.report_action_id.report_name)
