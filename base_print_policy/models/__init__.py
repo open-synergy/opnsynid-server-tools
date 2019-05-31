1ffc3d1e135bb4b5cfcc138ebda48610066c822c
Andhitia Rama <andhitia.r@gmail.com>
2019-05-31 21:40:07 +0700

    [8.0.1.0.0] base_print_policy
    * QC

--- base_print_policy/models/__init__.py
+++ base_print_policy/models/__init__.py
@@ -2,4 +2,6 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import base_print_policy
+from . import (
+    base_print_policy,
+)
