7172a3d7f30f4b0326eab4a421ecb86a1fd63083
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-11 08:28:09 +0700

    [8.0.1.1.0][base_print_policy]

--- base_print_policy/__openerp__.py
+++ base_print_policy/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Base Print Policy",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Base",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
