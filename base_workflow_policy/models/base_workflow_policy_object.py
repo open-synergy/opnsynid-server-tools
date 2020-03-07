9f234e4f9d6aa9bb6f045f0438a2791fdc342db4
mikevhe18 <viriyananda.michael@gmail.com>
2020-03-07 22:38:57 +0700

    [11.0.1.0.0][base_workflow_policy] Migration from 8.0

--- base_workflow_policy/models/base_workflow_policy_object.py
+++ base_workflow_policy/models/base_workflow_policy_object.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 OpenSynergy Indonesia
+# Copyright 2020 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, SUPERUSER_ID
+from odoo import models, api, SUPERUSER_ID
 
 
 class BaseWorkflowPolicyObject(models.AbstractModel):
@@ -13,7 +13,7 @@ class BaseWorkflowPolicyObject(models.AbstractModel):
     def _compute_policy(self):
         obj_line = self.env["base.workflow_policy_line"]
         criteria = [
-            ("workflow_id.model_id.model", "=", str(self._model)),
+            ("workflow_id.model_id.model", "=", str(self._name)),
         ]
         policy_lines = obj_line.search(criteria)
         for document in self:
