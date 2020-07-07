67154ec73668a17259e13a5704daf7397b1e2d2f
mikevhe18 <viriyananda.michael@gmail.com>
2020-07-07 17:13:41 +0700

    [8.0.2.4.0][base_multiple_approval]

--- base_multiple_approval/__openerp__.py
+++ base_multiple_approval/__openerp__.py
@@ -6,7 +6,7 @@
 {
     "name": "Base Multiple Approval",
     "summary": "Implement a validation process based on tiers.",
-    "version": "8.0.2.3.0",
+    "version": "8.0.2.4.0",
     "category": "Tools",
     "website": "https://simetri-sinergi.id",
     "author": "Eficent, Odoo Community Association (OCA), "
@@ -16,7 +16,7 @@
     "installable": True,
     "depends": [
         "base",
-        "mail",
+        "email_template",
     ],
     "data": [
         "security/ir.model.access.csv",
