a770f8813d0ad8ea44010560152943d57bba4865
Andhitia Rama <andhitia.r@gmail.com>
2020-12-01 21:45:23 +0700

    [date_range_calendar_data] pre-commit fix

--- date_range_calendar_data/__openerp__.py
+++ date_range_calendar_data/__openerp__.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Date Range Data - Calendar",
     "version": "8.0.1.0.0",
-    "author": "OpenSynergy Indonesia",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "category": "Generic Modules/Base",
-    "website": "https://opensynergy-indonesia.com",
-    "depends": [
-        "date_range"
-    ],
-    "data": [
-        "data/date_range_type_data.xml"
-    ],
+    "website": "https://simetri-sinergi.id",
+    "depends": ["date_range"],
+    "data": ["data/date_range_type_data.xml"],
     "installable": True,
     "license": "AGPL-3",
 }
