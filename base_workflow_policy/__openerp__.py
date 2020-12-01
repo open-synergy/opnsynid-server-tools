2d0a0d4cb849dc02bc19852029c44e97110450e3
Andhitia Rama <andhitia.r@gmail.com>
2020-12-01 21:46:42 +0700

    [base_workflow_policy] pre-commit fix

--- base_workflow_policy/__openerp__.py
+++ base_workflow_policy/__openerp__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
@@ -6,8 +5,8 @@
     "name": "Abstract Model for Workflow Policy",
     "version": "8.0.1.0.1",
     "category": "Administration",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
