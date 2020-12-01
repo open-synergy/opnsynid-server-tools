ae54d97aad5367e7f4e264f17586a403497f7b6f
Andhitia Rama <andhitia.r@gmail.com>
2020-12-01 21:54:19 +0700

    [base_copy_user_access] pre-commit fix

--- base_copy_user_access/__openerp__.py
+++ base_copy_user_access/__openerp__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Base Copy User Access',
-    'version': '8.0.1.0.0',
-    'summary': 'Copy access right from another user',
-    "author": "OpenSynergy Indonesia",
-    'category': 'Generic Modules/Base',
-    'website': 'https://opensynergy-indonesia.com',
-    'depends': ['base'],
-    'data': ['wizards/base_copy_user_access.xml'],
-    'installable': True,
-    'license': 'AGPL-3',
+    "name": "Base Copy User Access",
+    "version": "8.0.1.0.0",
+    "summary": "Copy access right from another user",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "category": "Generic Modules/Base",
+    "website": "https://simetri-sinergi.id",
+    "depends": ["base"],
+    "data": ["wizards/base_copy_user_access.xml"],
+    "installable": True,
+    "license": "AGPL-3",
 }
