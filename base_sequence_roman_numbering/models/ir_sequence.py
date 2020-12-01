e0bafda70fd7b8960cfc2583b34ec898da446c56
Andhitia Rama <andhitia.r@gmail.com>
2020-12-01 21:49:49 +0700

    [base_sequence_roman_numbering] pre-commit fix

--- base_sequence_roman_numbering/models/ir_sequence.py
+++ base_sequence_roman_numbering/models/ir_sequence.py
@@ -1,23 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models
 
 FORMATROMAN = (
-    ('M',  1000),
-    ('CM', 900),
-    ('D',  500),
-    ('CD', 400),
-    ('C',  100),
-    ('XC', 90),
-    ('L',  50),
-    ('XL', 40),
-    ('X',  10),
-    ('IX', 9),
-    ('V',  5),
-    ('IV', 4),
-    ('I',  1)
+    ("M", 1000),
+    ("CM", 900),
+    ("D", 500),
+    ("CD", 400),
+    ("C", 100),
+    ("XC", 90),
+    ("L", 50),
+    ("XL", 40),
+    ("X", 10),
+    ("IX", 9),
+    ("V", 5),
+    ("IV", 4),
+    ("I", 1),
 )
 
 
@@ -35,7 +34,6 @@ class IrSequence(models.Model):
     def _interpolation_dict_context(self, context=None):
         res = super(IrSequence, self)._interpolation_dict_context(context)
         dict_roman = res.copy()
-        for key, value in res.iteritems():
-            dict_roman['rom_' + key] =\
-                self._convert_to_roman(int(value))
+        for key, value in res.iteritems():  # noqa: B301
+            dict_roman["rom_" + key] = self._convert_to_roman(int(value))
         return dict_roman
