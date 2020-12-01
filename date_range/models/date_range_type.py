c32a2f1e6d54d2a946927120f20bf962a9f89165
Andhitia Rama <andhitia.r@gmail.com>
2020-12-01 21:46:15 +0700

    [date_range] pre-commit fix

--- date_range/models/date_range_type.py
+++ date_range/models/date_range_type.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -10,19 +9,25 @@ class DateRangeType(models.Model):
 
     @api.model
     def _default_company(self):
-        return self.env['res.company']._company_default_get('date.range')
+        return self.env["res.company"]._company_default_get("date.range")
 
     name = fields.Char(required=True, translate=True)
     allow_overlap = fields.Boolean(
-        help="If sets date range of same type must not overlap.",
-        default=False)
+        help="If sets date range of same type must not overlap.", default=False
+    )
     active = fields.Boolean(
         help="The active field allows you to hide the date range without "
-        "removing it.", default=True)
+        "removing it.",
+        default=True,
+    )
     company_id = fields.Many2one(
-        comodel_name='res.company', string='Company', select=1,
-        default=_default_company)
+        comodel_name="res.company", string="Company", select=1, default=_default_company
+    )
 
     _sql_constraints = [
-        ('date_range_type_uniq', 'unique (name,company_id)',
-         'A date range type must be unique per company !')]
+        (
+            "date_range_type_uniq",
+            "unique (name,company_id)",
+            "A date range type must be unique per company !",
+        )
+    ]
--- date_range/tests/test_date_range_type.py
+++ date_range/tests/test_date_range_type.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,15 +5,13 @@ from openerp.tests.common import TransactionCase
 
 
 class DateRangeTypeTest(TransactionCase):
-
     def test_default_company(self):
-        drt = self.env['date.range.type'].create(
-            {'name': 'Fiscal year',
-             'allow_overlap': False})
+        drt = self.env["date.range.type"].create(
+            {"name": "Fiscal year", "allow_overlap": False}
+        )
         self.assertTrue(drt.company_id)
         # you can specify company_id to False
-        drt = self.env['date.range.type'].create(
-            {'name': 'Fiscal year',
-             'company_id': False,
-             'allow_overlap': False})
+        drt = self.env["date.range.type"].create(
+            {"name": "Fiscal year", "company_id": False, "allow_overlap": False}
+        )
         self.assertFalse(drt.company_id)
