fbc3df29da030bf4709b6dce03dd9c3963272f68
Nurazmi <azmimr67@gmail.com>
2022-02-11 16:42:44 +0700

    [base_copy_user_access] 14.0.1.0.0
    * Task T/20/001245 - 14.0.1.0.0
    * Pembuatan Awal

--- base_copy_user_access/__manifest__.py
+++ base_copy_user_access/__manifest__.py
@@ -9,7 +9,7 @@
     "summary": "Copy access right from another user",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "category": "Generic Modules/Base",
-    "website": "https://simetri-sinergi.id",
+    "website": "https://github.com/OCA/vertical-rental",
     "depends": ["base"],
     "data": [
         "wizards/base_copy_user_access.xml",
