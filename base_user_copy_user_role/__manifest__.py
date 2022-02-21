d5ba60cbe0b1b93c4cdc45fd913c26ec5f6e9bff
Nurazmi <azmimr67@gmail.com>
2022-02-21 15:42:15 +0700

    [base_user_copy_user_role] 14.0.1.0.0
    * Task 14.0-T001249
    * Pembuatan Awal

---
+++ base_user_copy_user_role/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Base Copy User Role",
+    "version": "14.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "category": "Generic Modules/Base",
+    "website": "https://github.com/OCA/vertical-rental",
+    "depends": ["base_user_role"],
+    "data": [
+        "wizards/base_copy_user_role.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
