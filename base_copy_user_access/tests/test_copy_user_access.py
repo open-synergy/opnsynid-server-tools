9c6c1016ac7f67a9ccf2ebf614a80083439685e7
Nurazmi <azmimr67@gmail.com>
2022-03-07 15:03:10 +0700

    [base_copy_user_access] 14.0.1.0.0
    * Revisi after run  pre-commit

--- base_copy_user_access/tests/test_copy_user_access.py
+++ base_copy_user_access/tests/test_copy_user_access.py
@@ -1,29 +1,24 @@
-# -*- coding: utf-8 -*-
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import TransactionCase
 from lxml import etree
+
+from odoo.tests.common import TransactionCase
 
 
 class TestCopyUserAccess(TransactionCase):
-
     def setUp(self, *args, **kwargs):
         super(TestCopyUserAccess, self).setUp(*args, **kwargs)
 
         # Objects
-        self.obj_res_users = self.env['res.users']
-        self.obj_wizard = self.env['base.copy_user_access']
+        self.obj_res_users = self.env["res.users"]
+        self.obj_wizard = self.env["base.copy_user_access"]
 
         # Data
-        self.demo_user = self.env.ref('base.user_demo')
+        self.demo_user = self.env.ref("base.user_demo")
 
     def _prepare_user_data(self):
-        data = {
-            'login': 'test_user@test.com',
-            'name': 'test lagi',
-            'password': 'a'
-        }
+        data = {"login": "test_user@test.com", "name": "test lagi", "password": "a"}
 
         return data
 
@@ -37,22 +32,22 @@ class TestCopyUserAccess(TransactionCase):
         # Fill Context
         context = self.obj_res_users.context_get()
         ctx = context.copy()
-        ctx.update({'active_ids': user.ids})
+        ctx.update({"active_ids": user.ids})
 
         # Create Wizard
-        wizard = self.obj_wizard\
-            .with_context(ctx).create({'user_id': self.demo_user.id})
+        wizard = self.obj_wizard.with_context(ctx).create(
+            {"user_id": self.demo_user.id}
+        )
 
         # Check fields_view_get
         view = wizard.fields_view_get()
 
-        doc = etree.XML(view['arch'])
+        doc = etree.XML(view["arch"])
         for node in doc.xpath("//field[@name='user_id']"):
-            domain = node.get('domain')
+            domain = node.get("domain")
             test_domain = "[('id', 'not in', " + str(user.ids) + ")]"
             self.assertEquals(domain, test_domain)
 
         # Check group_ids(new_user) with group_ids(demo_user)
         wizard.with_context(ctx).copy_access_right()
-        self.assertEquals(set(self.demo_user.groups_id.ids),
-                          set(user.groups_id.ids))
+        self.assertEquals(set(self.demo_user.groups_id.ids), set(user.groups_id.ids))
--- base_copy_user_access/wizards/base_copy_user_access.py
+++ base_copy_user_access/wizards/base_copy_user_access.py
@@ -2,43 +2,40 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
 from lxml import etree
+
+from odoo import api, fields, models
 
 
 class WizardBaseCopyUserAccess(models.TransientModel):
-    _name = 'base.copy_user_access'
-    _description = 'Wizard Copy User Access'
+    _name = "base.copy_user_access"
+    _description = "Wizard Copy User Access"
 
-    user_id = fields.Many2one(
-        string='User',
-        comodel_name='res.users',
-        required=True
-        )
+    user_id = fields.Many2one(string="User", comodel_name="res.users", required=True)
 
     @api.model
     def fields_view_get(
-        self, view_id=None, view_type='form', toolbar=False, submenu=False
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
     ):
         res = super(WizardBaseCopyUserAccess, self).fields_view_get(
-            view_id=view_id, view_type=view_type,
-            toolbar=toolbar, submenu=submenu)
-        doc = etree.XML(res['arch'])
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+        )
+        doc = etree.XML(res["arch"])
         for node in doc.xpath("//field[@name='user_id']"):
-            active_ids = self._context.get('active_ids')
+            active_ids = self._context.get("active_ids")
             domain = "[('id', 'not in', " + str(active_ids) + ")]"
-            node.set('domain', domain)
-        res['arch'] = etree.tostring(doc)
+            node.set("domain", domain)
+        res["arch"] = etree.tostring(doc)
         return res
 
     def copy_access_right(self):
         res = []
         self.ensure_one()
 
-        obj_user = self.env['res.users']
+        obj_user = self.env["res.users"]
 
         context = self._context
-        record_id = context['active_ids']
+        record_id = context["active_ids"]
 
         user = obj_user.browse(self.user_id.id)
 
@@ -48,9 +45,9 @@ class WizardBaseCopyUserAccess(models.TransientModel):
         for data in record_id:
             user_id = obj_user.browse(data)
             vals = {
-                'groups_id': [(6, 0, res)],
-                }
+                "groups_id": [(6, 0, res)],
+            }
 
             user_id.write(vals)
 
-        return {'type': 'ir.actions.act_window_close'}
+        return {"type": "ir.actions.act_window_close"}
