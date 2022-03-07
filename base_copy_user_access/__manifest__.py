139981abd2f2bc1e18e211ce6b2c562f8bc2f304
mikevhe18 <viriyananda.michael@gmail.com>
2022-03-07 15:45:24 +0700

    [opnsynid-server-tools] pre-commit fix

--- base_copy_user_access/__manifest__.py
+++ base_copy_user_access/__manifest__.py
@@ -9,7 +9,7 @@
     "summary": "Copy access right from another user",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "category": "Generic Modules/Base",
-    "website": "https://github.com/OCA/vertical-rental",
+    "website": "https://simetri-sinergi.id",
     "depends": ["base"],
     "data": [
         "wizards/base_copy_user_access.xml",
