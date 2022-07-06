5c9cb1f3567835b95ce901b7f54ece9a37c96a1a
mikevhe18 <viriyananda.michael@gmail.com>
2022-07-06 15:09:17 +0700

    [base_duration] 14.0.1.0.0

---
+++ base_duration/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    models,
+)
---
+++ base_duration/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Base Duration",
+    "version": "14.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "category": "Generic Modules/Base",
+    "website": "https://simetri-sinergi.id",
+    "depends": [
+        "base_public_holiday",
+        "ssi_master_data_mixin",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/base_duration_views.xml",
+    ],
+    "installable": True,
+    "license": "LGPL-3",
+}
