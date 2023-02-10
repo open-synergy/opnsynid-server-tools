beab81b469538daf5990d71ca843dfc3b5e09b33
Andhitia Rama <andhitia.r@gmail.com>
2023-02-10 22:47:40 +0700

    [base_duration] 14.0.1.1.0

--- base_duration/models/base_duration.py
+++ base_duration/models/base_duration.py
@@ -2,6 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
 
+from dateutil.relativedelta import relativedelta
 from dateutil.rrule import DAILY, FR, MO, SA, SU, TH, TU, WE, rrule
 
 from odoo import fields, models
@@ -14,6 +15,38 @@ class BaseDuration(models.Model):
     ]
     _description = "Base Duration"
 
+    relative_delta_year = fields.Integer(
+        string="Year",
+        default=0,
+    )
+    relative_delta_month = fields.Integer(
+        string="Month",
+        default=0,
+    )
+    relative_delta_day = fields.Integer(
+        string="Day",
+        default=0,
+    )
+    relative_delta_years = fields.Integer(
+        string="Years",
+        default=0,
+    )
+    relative_delta_months = fields.Integer(
+        string="Months",
+        default=0,
+    )
+    relative_delta_weeks = fields.Integer(
+        string="Weeks",
+        default=0,
+    )
+    relative_delta_days = fields.Integer(
+        string="Days",
+        default=0,
+    )
+    relative_delta_weekday = fields.Integer(
+        string="Weekday",
+        default=0,
+    )
     number_of_days = fields.Integer(
         string="Number of Days",
         required=True,
@@ -43,6 +76,16 @@ class BaseDuration(models.Model):
 
         self.ensure_one()
         date_value = date_value or fields.Date.context_today(self)
+        date_value = date_value + relativedelta(
+            year=self.relative_delta_year,
+            month=self.relative_delta_month,
+            day=self.relative_delta_day,
+            years=self.relative_delta_years,
+            months=self.relative_delta_months,
+            weeks=self.relative_delta_weeks,
+            days=self.relative_delta_days,
+            weekday=self.relative_delta_weekday,
+        )
         result = fields.Date.from_string(date_value)
 
         if self.number_of_days > 0:
