f9ba05548066e33a3b7bc812815c69ed66298ae9
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-20 14:51:00 +0700

    [base_user_role_menu] 14.0.1.0.1

--- base_user_role_menu/__manifest__.py
+++ base_user_role_menu/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Manage Menu Access Based on Roles",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "category": "Generic Modules/Base",
     "website": "https://simetri-sinergi.id",
