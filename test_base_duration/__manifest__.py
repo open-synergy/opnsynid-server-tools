b07eca465d89e3571373752d0adc6a70b488da6d
mikevhe18 <viriyananda.michael@gmail.com>
2024-03-04 16:05:53 +0700

    [UPD] Change License to AGPL

--- test_base_duration/__manifest__.py
+++ test_base_duration/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Test Module - Base Duration",
     "version": "14.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "category": "Tools",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "base_duration",
