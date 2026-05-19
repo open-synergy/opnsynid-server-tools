a97b642222bb58ff03ab14727c0af799ce1aea11
Andhitia Rama <andhitia.r@gmail.com>
2026-05-19 07:13:36 +0700

    [UPD] test_base_duration
    * Add unit tests
    * Fix manifest pylint disable

--- test_base_duration/__manifest__.py
+++ test_base_duration/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Test Module - Base Duration",
     "version": "14.0.1.0.2",
