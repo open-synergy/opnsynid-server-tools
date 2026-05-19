b0dd04204d2f7a30688b9f4eab1a711bc6ecda04
Andhitia Rama <andhitia.r@gmail.com>
2026-05-19 07:13:25 +0700

    [UPD] ssi_frequency
    * Add unit tests
    * Fix view priority for replace
    * Fix manifest pylint disable

--- ssi_frequency/__init__.py
+++ ssi_frequency/__init__.py
@@ -4,4 +4,5 @@
 
 from . import (
     models,
+    tests,
 )
--- ssi_frequency/__manifest__.py
+++ ssi_frequency/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Frequency",
     "version": "14.0.1.1.0",
