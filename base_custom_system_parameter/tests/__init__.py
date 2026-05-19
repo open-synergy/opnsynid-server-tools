9192a9a413586dac29eb089b9188c59b252853e7
Andhitia Rama <andhitia.r@gmail.com>
2026-05-19 07:11:31 +0700

    [UPD] base_custom_system_parameter
    * Add unit tests
    * Fix deprecated tree string attribute in views

---
+++ base_custom_system_parameter/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from . import test_base_custom_system_parameter
