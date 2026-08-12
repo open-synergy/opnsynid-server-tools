9b782339ed9437f8108042818155acc8ccbadbd8
Andhitia Rama <andhitia.r@gmail.com>
2026-08-12 14:45:37 +0700

    [ADD] ssi_user_group_m2m
    Menambahkan modul ssi_user_group_m2m yang menampilkan field groups_id
    sebagai widget many2many biasa (dengan dialog Add yang bisa dicari) di
    tab Access Rights form res.users, berdampingan dengan blok reified
    checkbox/selection bawaan Odoo core. Menjawab kebutuhan admin mencari
    hak akses satu dokumen tanpa menggulir ratusan checkbox tanpa kotak
    pencarian.
    
    Modul XML-only (satu view inherit atas base.view_users_form), tanpa
    model Python. Blok reified bawaan tidak dimatikan, sesuai Keputusan
    Desain issue #107.
    
    Closes open-synergy/opnsynid-server-tools#107

---
+++ ssi_user_group_m2m/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
---
+++ ssi_user_group_m2m/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "User Group Many2many",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_users_views.xml",
+    ],
+    "demo": [],
+}
